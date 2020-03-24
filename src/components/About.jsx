cb79baffe83a04720c029a8f544902e3baa22c01
smilekids <stingerzas001@gmail.com>
2020-03-24 12:03:11 +0700

    1 commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={process.env.PUBLIC_URL+'/andreas-klassen.jpg'} className='card-img-top' alt="..." />
+          <img src='https://mis.cmu.ac.th/cmumis/images/000000000000/images/20200324/802983cb-425f-45ab-83d1-c80d1a7a258f.jpg' width='250' height='250' className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Dome Potikanond (610610999)</p>
+            <p>Chinnskrit NGeonsom (610610577)</p>
             <p>This app use Google Firebase as backend.</p>
             <Link to="/">
               <h3>
